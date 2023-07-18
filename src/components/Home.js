4edae0fd1aec22d13171c21364895d7bf11dec4f
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-18 18:43:32 +0300

    complete assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { username, city } from "./data/user";
 
-function Home() {
+function Home(){
   return (
     <div id="home">
       <h1>
@@ -8,4 +9,6 @@ function Home() {
       </h1>
     </div>
   );
-}
+};
+  
+export default Home;
